093e4e4801ebb9b2f6ce3947395b3e77ddfaa75c
Karol Alzate <karol.alzate@udea.edu.co>
2021-09-11 17:49:49 -0500

    fix too many connections issue

--- services/db.js
+++ services/db.js
@@ -1,22 +1,15 @@
 var mysql = require('mysql');
 var config = require('../config')
 
-/*async function query(sql, callback) {
-  const connection = await mysql.createConnection(config.db);
-  connection.query(sql, (err, rows) => {
-    if(err) throw err;
-    return callback(rows);
-  });
-}*/
+const pool = mysql.createPool(config.db);
 
 async function query(sql, callback) {
-  const pool = await mysql.createPool(config.db);
   //pool.getConnection(function(err, connection) {
     pool.query(sql, (err, rows) => {
       if(err) throw err;
       return callback(rows);
     });
-  //}
+  //});
 }
 
 module.exports = {
